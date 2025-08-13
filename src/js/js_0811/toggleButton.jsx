899284d8571c605859e10520e3ccdf32e78dfd2e
huch99 <huichan58@naver.com>
2025-08-13 09:12:54 +0900

    Update toggleButton.jsx

--- src/js/js_0811/toggleButton.jsx
+++ src/js/js_0811/toggleButton.jsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 export default function ToggleButton() {
     const [isOn, setIsOn] = useState(false);
     const bodyColor = document.querySelector('body').style;
+    bodyColor.backgroundColor = isOn ? 'black' : 'white';
+    bodyColor.color = isOn ? 'white' : 'black';
 
     return (
         <>
             <h3>Boolean 값의 상태 변화 체크</h3>
             <button onClick={() => {
                 setIsOn(!isOn);
-                bodyColor.backgroundColor = isOn ? 'black' : 'white';
-                bodyColor.color = isOn ? 'white' : 'black';
             }}>
                 {isOn ? 'ON' : 'OFF'}
             </button>
