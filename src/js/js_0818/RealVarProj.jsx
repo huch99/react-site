51440270f4515428d3b68dc27419c329e3423df7
huch99 <huichan58@naver.com>
2025-08-18 12:28:40 +0900

    Update RealVarProj.jsx

--- src/js/js_0818/RealVarProj.jsx
+++ src/js/js_0818/RealVarProj.jsx
@@ -7,11 +7,13 @@ const RealVarProj = () => {
     return (
         <>
             <VarProj01>
+                <p>useGlobal을 활용했기에 두 state는 공유됨.</p>
                 <Ex_context__01/>
                 <Ex_context_02/>
             </VarProj01>
             <hr />
             <VarProj01>
+                <p>useGlobal을 활용했지만 다른 객체로 나누어져 있어 공유되지 않음.</p>
                 <Ex_context_02/>
             </VarProj01>
         </>
